13aff114b84a8c7864b1b7fc63b634c9ed6a7593
Paul Thompson <paulthom12345@gmail.com>
2015-07-15 19:09:02 +1000

    Blur events on click away

--- public/javascripts/result.js
+++ public/javascripts/result.js
@@ -1,11 +1,14 @@
 var reader = new commonmark.Parser();
 var writer = new commonmark.HtmlRenderer();
 var inp;
+var popup;
 
 document.addEventListener('DOMContentLoaded', onLoad, false);
 
 function onLoad() {
     inp = document.getElementById("hiddenText")
+    popup = document.getElementById("popup");
+
     result = writer.render(reader.parse(inp.getAttribute("value")))
     document.getElementById('render').innerHTML = result
     $('pre code').each(function(i, block) {
@@ -13,12 +16,20 @@ function onLoad() {
     });
 
     document.getElementById("shorturl").addEventListener("click", onShortLink, false);
+    registerBlurEvents()
 }
 
 function onShortLink() {
-    document.getElementById("popup").style.display = "block"
+    popup.style.display = "block"
     var input = document.getElementById("autoselect");
     input.focus();
     input.select();
 }
 
+function doBlur() {
+    popup.style.display = "none"
+}
+
+function registerBlurEvents() {
+    document.getElementById("render").addEventListener("click", doBlur, false);
+}
